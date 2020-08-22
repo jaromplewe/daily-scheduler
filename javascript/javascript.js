051335bbccaff571d72dea3f8b2bb2fccfcdbc2d
Jarom Plewe <jplewe23@gmail.com>
2020-08-22 09:26:49 -0600

    save button works and textarea is stored to local storage

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,14 +1,50 @@
-
-var m = moment().format("dddd" + ", " + "MMMM" + " " + "Do"); 
+// APPEND THE CURRENT DATE
+let m = moment().format("dddd" + ", " + "MMMM" + " " + "Do");
 $('#currentDay').append(m);
 
-$('.fa-save').on('click', function() {
-    console.log($('.textarea').val())
-}); 
-// let hours = $('.hour').text();
-// console.log([hours])
+// CREATE HOUR VARIABLES FOR COMPARISON
+let textHour = $('.textHour');
+let currentHour = JSON.stringify(moment().hour());
+// console.log(moment().hour("8:00 PM"))
 
+let textMoment = [];
+for (let i = 0; i < $('.textHour').length; i++) {
+    let wowo = (moment(textHour[i].textContent).hour());
+    // console.log(wowo);
+    textMoment += i;
+};
 
-// if (moment().hour() === w.hour()) {
-    // console.log('butthole')
+// COMPARE THE TWO HOUR VARIABLES
+// console.log(JSON.stringify(moment()))
+// if (moment().hour() === textHour[0].hour()) {
+//     console.log('butthole')
 // }
+
+
+
+// SAVE TEXT TO LOCALSTORAGE
+
+// Get elements needed
+const hourClass = document.getElementsByClassName('textHour');
+const textArea = document.getElementsByClassName('textarea');
+const saveBtn = document.getElementsByClassName('saveBtn');
+// Add event listener to the save button to setItem to localStorage
+for (let i = 0; i < textArea.length; i++) {
+    saveBtn[i].addEventListener('click', function () {
+        localStorage.setItem(hourClass[i].textContent, textArea[i].value);
+    });
+}
+
+
+
+// APPEND THE LOCALSTORAGE TEXT INTO THE TEXT AREAS
+
+for (let i=0; i<textArea.length; i++) {
+    if (localStorage.getItem(hourClass[i].textContent) === null) {
+        console.log('butthole');
+    } else {
+    let locStoreText = localStorage.getItem(hourClass[i].textContent);
+    console.log(locStoreText)
+    textArea[i].textContent = locStoreText;
+    }
+}
